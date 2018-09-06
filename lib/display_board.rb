adc2c4e98200d8329935b9480bb11b4b3ae7b972
student online <eliel.badon@gmail.com>
2018-09-06 21:33:06 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,9 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board()
-  puts "#{0}|#{0}|#{0} "
+  puts "#{0}|#{1}|#{2} "
   puts "-----------"
-  puts "#{0}|#{0}|   "
+  puts "#{3}|#{4}|#{5}"
   puts "-----------"
-  puts "   |   |   "
+  puts "#{6}|#{7}|#{8}"
 end
